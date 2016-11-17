bf2af6596b58265261bbfc9cd01864939d5003b2
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-11-17 20:29:08 +0700

    Fix inline style

--- app/components/LazyLoading.js
+++ app/components/LazyLoading.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Loading from 'react-loading';
+const styles = {
+  position: 'absolute',
+  left: `50%`,
+  right: `50%`,
+  margin: `-30px 0 0 -20px`
+}
 
 const LazyLoading = () => {
   return (
-    <div style={{marginLeft: 10 + 'em'}}>
+    <div style={styles}>
       <Loading type='bars' color='#E2E2E2' />
     </div>
   );
